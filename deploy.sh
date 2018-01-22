f3d9f67ff98d221612479cd36f636238765f4674
Miya Sakura <3839ra@gmail.com>
2018-01-22 23:38:19 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 ./node_modules/.bin/webpack
-aws cloudformation update-stack --profile hinomi-prod --template-body file://cloudformation.yaml --stack-name ShowroomCommentViewer
-aws s3 sync ./  s3://sr-comment-and-gift-viewer/ --profile hinomi-prod --exclude "*" --include "*.png" --include "*.css" --include "*.html" --include "*.js" --exclude "node_modules/*" --exclude "webpack.config.js" --exclude "src/*"
-aws cloudfront --profile hinomi-prod create-invalidation --distribution-id ER44UNV8EX7H2 --paths '/*'
+aws s3 sync ./  s3://sr-viewer.sacra.co/ --profile hinomi-prod --exclude "*" --include "*.png" --include "*.css" --include "*.html" --include "*.js" --exclude "node_modules/*" --exclude "webpack.config.js" --exclude "src/*"
